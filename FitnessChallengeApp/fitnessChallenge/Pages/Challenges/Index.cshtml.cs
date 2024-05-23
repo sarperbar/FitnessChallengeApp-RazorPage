05004d8cbb34a81039eb5df9dc4629b2e437205a
Sarper ERBAR <c2011001@student.cankaya.edu.tr>
2024-05-23 17:07:57 +0300

    challenges bitti

--- FitnessChallengeApp/fitnessChallenge/Pages/Challenges/Index.cshtml.cs
+++ FitnessChallengeApp/fitnessChallenge/Pages/Challenges/Index.cshtml.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using fitnessChallenge.Data;
 using fitnessChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace fitnessChallenge.Pages.Challenges
@@ -18,9 +20,39 @@ namespace fitnessChallenge.Pages.Challenges
 
         public IList<Challenge> Challenges { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public SearchModel Search { get; set; }
+
         public async Task OnGetAsync()
         {
-            Challenges = await _context.Challenges.ToListAsync();
+            var query = _context.Challenges.AsQueryable();
+
+            if (!string.IsNullOrEmpty(Search.Keyword))
+            {
+                query = query.Where(c => c.Title.Contains(Search.Keyword) || c.Description.Contains(Search.Keyword));
+            }
+
+            if (Search.Difficulty.HasValue)
+            {
+                query = query.Where(c => c.DifficultyLevel == Search.Difficulty.Value);
+            }
+
+            if (Search.Category.HasValue)
+            {
+                query = query.Where(c => c.Category == Search.Category.Value);
+            }
+
+            if (Search.StartDate.HasValue)
+            {
+                query = query.Where(c => c.StartDate >= Search.StartDate.Value);
+            }
+
+            if (Search.EndDate.HasValue)
+            {
+                query = query.Where(c => c.EndDate <= Search.EndDate.Value);
+            }
+
+            Challenges = await query.ToListAsync();
         }
     }
 }
