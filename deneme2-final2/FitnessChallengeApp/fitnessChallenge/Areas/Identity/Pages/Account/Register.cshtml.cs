81f56f265c281c9ca56234fc1aeb421da8fa389b
Sarper ERBAR <c2011001@student.cankaya.edu.tr>
2024-05-25 18:49:43 +0300

    son şeyler tamam

--- deneme2-final2/FitnessChallengeApp/fitnessChallenge/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ deneme2-final2/FitnessChallengeApp/fitnessChallenge/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -126,26 +126,19 @@ namespace fitnessChallenge.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var userId = await _userManager.GetUserIdAsync(user);
+                    // Email confirmation bypass
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    var confirmResult = await _userManager.ConfirmEmailAsync(user, code);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
+                    if (confirmResult.Succeeded)
                     {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
+                    }
+
+                    foreach (var error in confirmResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
                     }
                 }
                 foreach (var error in result.Errors)
